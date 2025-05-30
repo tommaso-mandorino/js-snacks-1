c62d839fb3517ab870220b8eaa75c02172e0e81e
tommaso-mandorino <tommasomandorino@outlook.it>
2025-05-30 13:47:21 +0200

    feat: add snack 6

--- snack-5/main.js
+++ snack-5/main.js
@@ -1,4 +1,4 @@
 const lastNumber = Number(prompt('Inserisci fino a che numero vuoi elevare al cubo:'));
 for (let i = 0; i <= lastNumber; i++) {
-    console.log(i**3);
+    console.log( i + '^3 = ' + i**3);
 }
